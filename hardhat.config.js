0df7dfaf1859d91bfe8d415cd83be27af44a0b78
Franco Aguilera <flaalf@gmail.com>
2022-09-28 18:50:29 -0300

    added staging testing for goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,18 +7,17 @@ require("hardhat-contract-sizer")
 require("dotenv").config()
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
-
-/*
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-*/
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
   gasReporter: {
     coinmarketcap: COINMARKETCAP_API_KEY,
     currency: "USD",
@@ -26,6 +25,9 @@ module.exports = {
     noColors: true,
     outputFile: "gas-report.txt",
     token: "MATIC",
+  },
+  mocha: {
+    timeout: 3600000, // 200 seconds max
   },
   namedAccounts: {
     deployer: {
@@ -37,7 +39,7 @@ module.exports = {
   },
   networks: {
     goerli: {
-      accounts: [GOERLI_PRIVATE_KEY],
+      accounts: [PRIVATE_KEY],
       blockConfirmations: 6,
       chainId: 5,
       url: GOERLI_RPC_URL,
@@ -47,14 +49,11 @@ module.exports = {
       chainId: 31337,
     },
     rinkeby: {
-      accounts: [RINKEBY_PRIVATE_KEY],
+      accounts: [PRIVATE_KEY],
       blockConfirmations: 6,
       chainId: 4,
       url: RINKEBY_RPC_URL,
     },
   },
   solidity: "0.8.8",
-  mocha: {
-    timeout: 200000, // 200 seconds max
-  },
 }
